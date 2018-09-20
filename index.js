a9d291151685d00595b8ab2c549a07c6583671e6
Marius Rumpf <marius@mariusrumpf.de>
2018-09-20 20:48:22 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const alfy = require('alfy');
+
+const output = [];
+
+function addOutput(title, subtitle, arg, action) {
+	output.push({
+		title,
+		subtitle,
+		arg,
+		variables: {
+			action
+		}
+	});
+}
+
+function addUuidOutput(uuid, version) {
+	output.push({
+		title: uuid,
+		subtitle: `UUID${version}, Actions:  ⏎ to copy`,
+		arg: uuid,
+		text: {
+			copy: uuid,
+			largetype: uuid
+		},
+		variables: {
+			action: 'copy'
+		}
+	});
+}
+
+function genUuids(generator, version, count = 4) {
+	for (let i = 0; i < count; i += 1) {
+		const uuid = generator();
+		addUuidOutput(uuid, version);
+	}
+}
+
+if (alfy.input.toLowerCase() === 'v1') {
+	genUuids(require('uuid/v1'), 'v1');
+} else if (alfy.input.toLowerCase() === 'v4') {
+	genUuids(require('uuid/v4'), 'v4');
+} else {
+	addOutput('Generate v4 UUIDs', '', `${process.env.keyword} v4`, 'rerun');
+	addOutput('Generate v1 UUIDs', '', `${process.env.keyword} v1`, 'rerun');
+}
+
+alfy.output(output);
---
+++ readme.md
@@ -0,0 +1,30 @@
+# alfred-uuid
+
+> [Alfred 3](https://www.alfredapp.com) workflow to generate UUIDs in v1 and v4
+
+<img src="screenshot.png" width="580">
+
+## Install
+
+```
+$ npm install --global alfred-uuid
+```
+
+*Requires [Node.js](https://nodejs.org) 8+ and the Alfred [Powerpack](https://www.alfredapp.com/powerpack/).*
+
+
+## Usage
+
+In Alfred, type `uid`, <kbd>⏎</kbd>
+- press <kbd>⏎</kbd> to copy a uuid.
+
+
+## Related
+
+- [uuid](https://github.com/kelektiv/node-uuid) - Package used to generate uuids
+
+## License
+
+MIT © [Marius Rumpf](https://github.com/mariusrumpf)
+
+<p align="center"><img src="icon.png" width="64"></p>
